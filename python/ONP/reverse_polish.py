55ba0cf0311d84ce98599b9a6dc63fcfa21040cd
michael.schiff <michael.schiff@tubemogul.com>
2014-09-02 15:24:04 -0700

    ONP accepted

--- python/ONP/reverse_polish.py
+++ python/ONP/reverse_polish.py
@@ -1,10 +1,18 @@
 import sys
 
+def next_matching_index(expr):
+    depth = 1
+    for (i, char) in enumerate(expr):
+        if char == '(': depth += 1
+        if char == ')': depth -= 1
+        if depth == 0: return i
+
+
 def parse(expr):
     expr = expr.strip()
     if len(expr) > 1:
         if expr[0] == '(':
-            closing = expr.rfind(')')
+            closing = next_matching_index(expr[1:]) + 1
             left = expr[1:closing]
             if closing + 1 == len(expr):
                 return (left, None, None)
@@ -19,13 +27,30 @@ def parse(expr):
     else:
         return (expr, None, None)
 
-def to_rpn(expr):
-    print parse(expr)
+def to_rpn(expr, so_far):
+    (left, op, right) = parse(expr)
+    if left:
+        if len(left) == 1:
+            so_far += left
+        else:
+            so_far = to_rpn(left, so_far)
+        
+    if right:
+        if right and len(right) == 1:
+            so_far += right
+        else:
+            so_far = to_rpn(right, so_far)
+
+    if op:
+        so_far += op
+
+    return so_far
+
 
 if __name__ == "__main__":
     num_cases = int(sys.stdin.readline())
     for case in range(num_cases):
-        to_rpn(sys.stdin.readline())
+        print(to_rpn(sys.stdin.readline(), ""))
 
 
 
