59d2cd5c94f0f920cc837dee2b12d6ebf6380e03
michael.schiff <michael.schiff@tubemogul.com>
2014-09-02 17:22:07 -0700

    proggress on PALIN

--- python/PALIN/next_palindrome.py
+++ python/PALIN/next_palindrome.py
@@ -1,10 +1,32 @@
 import math
 
 def next_pallindrome(num):
-    digits = list(int(d) for d in str(num))
+    fringe = [num]
     pallindromes = []
-    for i in range(len(digits)/2):
-        left, right = digits
+
+    # iterate through fringe
+
+    for i in range(num_digits(num)/2):
+        left_place = 10**i
+        left = digit_at_place(num, left_place)
+
+        right_place = 10**(num_digits(num)-i-1)
+        right = digit_at_place(num, right_place) 
+
+        smaller, larger = math.min(left, right), math.max(left, right)
+
+        for j in range(smaller, larger+1):
+            left_diff = (j - left) * left_place
+            right_diff = (j - right) * right_place
+            new_num = num
+            new_num += left_diff
+            new_num += right_diff
+            if is_pallindrome(new_num):
+                pallindromes.appned(new_num)
+            else:
+                fringe.append(new_num)
+
+
 
 def digit_at_place(num, i):
     return (num % (i*10)) / i
@@ -18,4 +40,5 @@ def is_pallindrome(num):
         right = digit_at_place(num, 10**(num_digits(num)-i-1))
         if left != right: return False
     return True
-    
+
+
